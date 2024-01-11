2e8287d4249c181cac1aac5bc037a03cb703d159
José Augusto Freire <aquarela.design@gmail.com>
2024-01-11 11:34:33 -0300

    Adicionado o pacote de componentes PrimeVue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import PrimeVue from 'primevue/config'
+import Card from 'primevue/card';
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+
+import 'primevue/resources/themes/bootstrap4-dark-blue/theme.css'
+
 const app = createApp(App)
 app.use(router)
 app.use(store)
-
+app.use(PrimeVue)
+app.use(ToastService)
+app.component('PCard', Card)
+app.component('PTabView', TabView)
+app.component('PTabPanel', TabPanel)
+app.component('PToast', Toast)
 app.mount('#app')
