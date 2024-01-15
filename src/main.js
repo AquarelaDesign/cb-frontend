ea139460f60f779fec85bb44b78ec58b7f472f31
José Augusto Freire <aquarela.design@gmail.com>
2024-01-15 04:32:10 -0300

    Inclusão do adapdador date-fns para o ChartJS

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { FontAwesomeIcon } from './plugins/font-awesome'
+import 'chartjs-adapter-date-fns';
 
 import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
